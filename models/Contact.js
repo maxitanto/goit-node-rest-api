85dd3e9336ab5156b019e673af91de84e693a70d
Maksym Baldyniuk <frundzik1989@gmail.com>
2024-02-27 22:35:55 +0200

    completed task

---
+++ models/Contact.js
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose';
+import Joi from 'joi';
+
+import { handleSaveError, preUpdate } from './hooks.js';
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+contactSchema.post('save', handleSaveError);
+contactSchema.pre('findOneAndUpdate', preUpdate);
+contactSchema.post('findOneAndUpdate', handleSaveError);
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.boolean(),
+});
+
+export const contactFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const Contact = model('contact', contactSchema);
+
+export default Contact;
