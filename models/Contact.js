38894b6cde60a80ed9c1720f0eee18d654006aa8
Maksym Baldyniuk <frundzik1989@gmail.com>
2024-03-12 22:50:28 +0200

    added file .env.example & versionKey to contactSchema

--- models/Contact.js
+++ models/Contact.js
@@ -3,22 +3,25 @@ import Joi from 'joi';
 
 import { handleSaveError, preUpdate } from './hooks.js';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false }
+);
 
 contactSchema.post('save', handleSaveError);
 contactSchema.pre('findOneAndUpdate', preUpdate);
